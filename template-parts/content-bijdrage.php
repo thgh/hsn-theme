ba74dbd330c9e80890c8890db9be7459338fd936
Thomas Ghysels <info@thomasg.be>
2019-05-05 17:23:43 +0200

    Add textual pages

--- template-parts/content-bijdrage.php
+++ template-parts/content-bijdrage.php
@@ -6,7 +6,9 @@
  *
  * @package hsn-theme
  */
-
+global $parent;
+$author = get_post_meta($post->ID, 'auteur', true);
+$title = $post->post_title;
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -32,25 +34,66 @@
   <?php hsn_theme_post_thumbnail(); ?>
 
   <div class="entry-content">
-    <?php
-    the_content( sprintf(
-      wp_kses(
-        /* translators: %s: Name of current post. Only visible to screen readers */
-        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'hsn-theme' ),
-        array(
-          'span' => array(
-            'class' => array(),
-          ),
-        )
-      ),
-      get_the_title()
-    ) );
+    <?php echo $author ?>
+    <a href="<?php echo get_permalink($parent) ?>"><?php echo $parent->post_title ?></a>
 
-    wp_link_pages( array(
-      'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'hsn-theme' ),
-      'after'  => '</div>',
-    ) );
-    ?>
+<?php
+global $wpdb;
+$isbn = get_post_meta($parent->ID, 'isbn', true);
+
+$page_first = (int) get_post_meta($post->ID, 'page_first', true);
+$page_last =(int)  get_post_meta($post->ID, 'page_last', true);
+
+
+  $pages = $wpdb->get_results(
+    $wpdb->prepare(
+      "
+    SELECT * FROM hsnbundels_paginas
+    WHERE isbn LIKE %s AND page >= %s AND page <= %s
+    ORDER BY page ASC
+  ",
+      $isbn,
+      $page_first,
+      $page_last
+    )
+  );
+
+if (!count($pages)) {
+  var_dump($isbn);
+  var_dump($page_first);
+  var_dump($page_last);
+}
+
+$prevPage = 'first';
+foreach ($pages as $key => $page) {
+  if ($page->page == $prevPage) {
+    continue;
+  }
+  $contents = $page->contents;
+  $pos = strpos($contents, $title);
+  if ($pos > 0) {
+    $contents = substr($contents, $pos + strlen($title));
+  }
+  echo '</pre>';
+  echo '<div class="textual">';
+  // echo '<div class="textual__num">' . $page->page . '</div>';
+  echo '<div class="textual__toggle"><button class=active>Tekst</button><button>Afbeelding</button></div>';
+
+  $dom = new DOMDocument;
+  @$dom->loadHTML($contents);
+  $xpath = new DOMXPath($dom);
+  $nodes = $xpath->query('//@*');
+  foreach ($nodes as $node) {
+      $node->parentNode->removeAttribute($node->nodeName);
+  }
+  echo $dom->saveHTML();
+  // echo $contents;
+  // echo strip_tags($contents, '<p>');
+  echo '</div>';
+  $prevPage = $page->page;
+}
+
+?>
   </div><!-- .entry-content -->
 
   <footer class="entry-footer">
